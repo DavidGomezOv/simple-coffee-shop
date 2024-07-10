a527c076b425bba4ea6ccf5dff961d4d58204023
DavidGomezOv <david182400@gmail.com>
2024-07-09 19:54:24 -0600

    Initial Commit

---
+++ simple-coffee-shop/simple_coffee_shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_coffee_shopApp.swift
+//  simple-coffee-shop
+//
+//  Created by David Gomez on 9/07/24.
+//
+
+import SwiftUI
+
+@main
+struct simple_coffee_shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
